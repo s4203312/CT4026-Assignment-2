5a6d55b27c8c556bb9dd42a2d69ca266467141b2
s4203312 <s4203312@glos.ac.uk>
2022-11-24 17:38:28 +0000

    Progress

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class Options : MonoBehaviour
 {
     float masterAudio;
-    [SerializeField]  private AudioSource[] speakers;       //A list of the audio sources in the scene (they need to be added by hand)
+    [SerializeField]  private AudioSource[] speakers;       //A list of the audio sources in the scene
 
     private void Awake()
     {
         masterAudio = GetFloat("volume");       //Collect the volume setting from player preferances
-        //speakers = FindObjectsOfType<AudioSource>();        //This will collect all of the objects with an audio source in the scene
+        speakers = FindObjectsOfType<AudioSource>();        //This will collect all of the objects with an audio source in the scene
         UpdateSpeakers(masterAudio);            //Set the volume of all speakers
     }
 
