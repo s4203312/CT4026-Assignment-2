d9f3ea2dbca18ca6b105777ad784336eb03f37fd
s4203312 <s4203312@glos.ac.uk>
2022-12-05 23:41:40 +0000

    Work

--- Assets/Scripts/Flippers.cs
+++ Assets/Scripts/Flippers.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Flippers : MonoBehaviour
 {
+    private GameObject pinball;
+    [SerializeField] public float power;
+
+    private void Start() {
+        pinball = GameObject.Find("Pinball");
+        power = 400;
+    }
     public void Flipper()
     {
-        transform.GetComponent<Animator>().SetTrigger("UpdateState");
+        
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Flipper" ) {
+            transform.GetComponent<Animator>().SetTrigger("UpdateState");
+            pinball.GetComponent<Rigidbody>().AddForce(Vector3.up * power);
+        }
     }
 }
